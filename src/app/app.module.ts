59fe3c599c616a947287025517669eebbb1a7976
jaquecantarino <jaquelinecantarino@outlook.com>
2021-08-26 16:08:05 -0300

    Criando tela de Cadastro Empresa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CadastroEmpresaComponent } from './cadastro-empresa/cadastro-empresa.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CadastroEmpresaComponent
   ],
   imports: [
     BrowserModule,
