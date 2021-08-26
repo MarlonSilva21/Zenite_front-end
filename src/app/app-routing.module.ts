0dff78aeb812f6df61fbf004e35161160415ee18
MarlonSilva21 <josemarlondasilva48@gmail.com>
2021-08-26 18:09:18 -0300

    Implementando tela de cadastro do professor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroProfessorComponent } from './cadastro-professor/cadastro-professor.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'cadastro-estudante', pathMatch: 'full'},
+
+  {path:'cadastro-estudante', component: CadastroComponent},
+  {path:'cadastro-professor', component: CadastroProfessorComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { CadastroProfessorComponent } from './cadastro-professor/cadastro-professor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroComponent
+    CadastroComponent,
+    CadastroProfessorComponent
   ],
   imports: [
     BrowserModule,
