0de799f614ceb1a2bef83adf10d6468e8acc3d6b
MarlonSilva21 <josemarlondasilva48@gmail.com>
2021-09-01 11:47:19 -0300

    configurando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroProfessorComponent } from './cadastro-professor/cadastro-professor.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import {CadastroEmpresaComponent} from "./cadastro-empresa/cadastro-empresa.component";
+import {EsqueceuSenhaComponent} from "./esqueceu-senha/esqueceu-senha.component";
+import {FooterComponent} from "./footer/footer.component";
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 const routes: Routes = [
 
-  {path: '', redirectTo: 'cadastro-estudante', pathMatch: 'full'},
-
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path:'navbar', component: NavbarComponent},
+  {path:'home', component: HomeComponent},
+  {path:'login', component: LoginComponent},
   {path:'cadastro-estudante', component: CadastroComponent},
-  {path:'cadastro-professor', component: CadastroProfessorComponent}
+  {path:'cadastro-professor', component: CadastroProfessorComponent},
+  {path:'cadastro-empresa', component: CadastroEmpresaComponent},
+  {path:'esqueceu-senha', component: EsqueceuSenhaComponent},
+  {path:'footer', component: FooterComponent}
 
 ];
 
