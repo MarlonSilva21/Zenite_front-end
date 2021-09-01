56f601adacb5df424d236f782e054203b72dd76e
jaquecantarino <jaquelinecantarino@outlook.com>
2021-09-01 09:52:58 -0300

    implementando home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CadastroEmpresaComponent } from './cadastro-empresa/cadastro-empresa.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CadastroEmpresaComponent
+    CadastroEmpresaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
