3a11be31f14ba9e8ab4535a5537be69edbd813d0
andrem91 <andrem166@gmail.com>
2021-09-09 12:21:05 -0300

    Tela de cursos criada e tela de homeLogado ajustada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { CadastroProfessorComponent } from './cadastro-professor/cadastro-professor.component';
 import { FooterComponent } from './footer/footer.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { FooterComponent } from './footer/footer.component';
     LoginComponent,
     EsqueceuSenhaComponent,
     FooterComponent,
-    HomeLogadoComponent
+    HomeLogadoComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
